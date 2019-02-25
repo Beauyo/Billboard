a3fe3706d6a144db39798816c22278d1be7d4627
Beau Hicken <Turbodiesel2500dee@hotmail.com>
2019-02-25 16:45:04 -0700

    update songs

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,13 +1,41 @@
 class SongsController < ApplicationController
+  before_action :set_song, only: [:show, :update, :edit, :destroy]
   def index
+    @songs = Song.all
   end
 
   def show
   end
 
   def new
+    @song = Song.new
+    render partial: "form"
   end
 
-  def edit
+  def create
+    @song = Song.new(songs.params)
+    if 
+      @song.save
+      redirect_to artist_path
+    else 
+      render partial: "form" 
   end
+
+
+  def edit
+    render partial: "form"
+  end
+
+  def delete
+    @song.destroy
+  end
+
+  private 
+  def set_song
+    @songs = Song.find(params[:id])
+  end
+
+
+  def song_params
+    params.require(:billboard).permit(:artist)
 end
